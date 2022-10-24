84a46f135679fbbd2be7ae7e84ab38c96fcf383d
Muhamad Zaki Raihan <toteyoku4636@gmail.com>
2022-10-24 13:29:29 +0700

    Tugas laravel rest-api Animal

---
+++ task-rest-api/routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\AnimalsController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/animals', [AnimalController::class, 'index']);
+
+Route::post('/animals', [AnimalController::class, 'store']);
+
+Route::put('/animals/{id}', [AnimalController::class, 'update']);
+
+Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
