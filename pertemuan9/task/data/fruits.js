ebe8c45487c985674ea50e9d3a9e566ac9280096
Muhamad Zaki Raihan <toteyoku4636@gmail.com>
2022-11-30 11:47:10 +0700

    Tugas pertemuan 9 Javascript

---
+++ pertemuan9/task/data/fruits.js
@@ -0,0 +1,9 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+ var fruits = ["Anggur", "Apel", "Jambu"];
+
+ // TODO 2: export variable fruits
+ module.exports = fruits;
